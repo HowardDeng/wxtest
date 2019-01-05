f0683f7d9de9f2bc209681eedc0a2d372cd6b0a6
dengjinghao <dengjinghao@kmerit.com>
2019-01-05 09:56:43 +0800

    二货留言 体验版

--- pages/weather/weather.js
+++ pages/weather/weather.js
@@ -38,54 +38,57 @@ Page({
           success:function(res){
             console.log(res);
             that.setData({
-              province: res.result.address_component.province,
-              city: res.result.address_component.city
+              province: res.result.address_component.province.replace('省',''),
+              city: res.result.address_component.city.replace('市', '')
             })
-            console.log("province " + that.data.province)
-            console.log("city " + that.data.city)
+            console.log("province " + that.data.province);
+            console.log("city " + that.data.city);
+
+            that.getnow(function (d) {//获取到数据的回调函数
+              wx.hideToast();
+
+              d.now["cond_src"] = "https://cdn.heweather.com/cond_icon/" + d.now.cond_code + ".png";
+              d.basic["update"] = d.update;
+              that.setData({ basic: d.basic, now: d.now })//更新数据，视图将同步更新
+              console.log(d);
+            });
+            that.getsuggestion(function (d) {
+              console.log(d);
+              var lifestyle = d.lifestyle;
+              var msuggestion = {};
+              lifestyle.forEach(function (item, index) {
+                if (item.type == "comf") {
+                  msuggestion["comf"] = item;
+                } else if (item.type == "drsg") {
+                  msuggestion["drsg"] = item;
+                } else if (item.type == "flu") {
+                  msuggestion["flu"] = item;
+                } else if (item.type == "trav") {
+                  msuggestion["trav"] = item;
+                } else if (item.type == "sport") {
+                  msuggestion["sport"] = item;
+                } else if (item.type == "uv") {
+                  msuggestion["uv"] = item;
+                } else if (item.type == "cw") {
+                  msuggestion["cw"] = item;
+                }
+                that.setData({ suggestion: msuggestion });
+              })
+            });
+
           }
         })
         },
     })
     
-    that.getnow(function(d){//获取到数据的回调函数
-      wx.hideToast();
-
-      d.now["cond_src"] ="https://cdn.heweather.com/cond_icon/"+d.now.cond_code+".png";
-      d.basic["update"] = d.update;
-      that.setData({basic:d.basic,now:d.now})//更新数据，视图将同步更新
-      console.log(d);
-    })
-    that.getsuggestion(function(d){
-      console.log(d);
-      var lifestyle = d.lifestyle;
-      var msuggestion = {};
-      lifestyle.forEach(function(item,index){
-        if(item.type == "comf"){
-          msuggestion["comf"] = item;
-        }else if(item.type == "drsg"){
-          msuggestion["drsg"] = item;
-        } else if (item.type =="flu"){
-          msuggestion["flu"] = item;
-        } else if (item.type == "trav") {
-          msuggestion["trav"] = item;
-        } else if (item.type == "sport") {
-          msuggestion["sport"] = item;
-        } else if (item.type == "uv") {
-          msuggestion["uv"] = item;
-        } else if (item.type == "cw") {
-          msuggestion["cw"] = item;
-        }
-        that.setData({suggestion:msuggestion});
-      })
-    })},
+    },
   //3、自定义页面方法：获取当前天气API
   getnow:function(fn){
     wx.request({//请求服务器，类似ajax
       url: 'https://www.xiaoffjj.com/wether', 
       data: {
-            city:this.city,
-            province:this.province,
+            city:this.data.city,
+            province:this.data.province,
             },//和风天气提供用户key，可自行注册获得
       header: {'Content-Type': 'application/json'},
       success: function(res) {fn(res.data.HeWeather6[0]);}//成功后将数据传给回调函数执行
